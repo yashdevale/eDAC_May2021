9a39bdc9014dbf0976d3988bd4137b548c4a8147
yashdevale <64496594+yashdevale@users.noreply.github.com>
2021-05-09 00:00:41 +0530

    Add files via upload

---
+++ Assignment_1/PatternFace.java
@@ -0,0 +1,10 @@
+class PatternFace{
+	public static void main(String[]args){
+	 System.out.println(" +\"\"\"\"\"+");
+	 System.out.println("[| o o |]");
+	 System.out.println(" |  ^  |");
+	 System.out.println(" | '-' |");
+	 System.out.println(" +-----+  ");
+	}
+}
+
---
+++ Assignment_1/PatternFlag.java
@@ -0,0 +1,17 @@
+class PatternFlag{
+	public static void main(String[]args){
+		for(int i=1;i<=9;i++){
+			if(i%2!=0){
+				System.out.println("* * * * * * ======================");
+			}
+			else{
+				System.out.println(" * * * * *  ======================");
+				
+			}
+			
+		}
+		for(int i=1;i<=6;i++){
+			System.out.println("==================================");
+		}
+	}
+}
